4f05202822b393f43910b38c28bdea3aa6a02a2b
Alexandr <all1992ex@gmail.com>
2017-05-24 11:59:52 +0400

    delete more

--- Prognoz/Form1.cpp
+++ Prognoz/Form1.cpp
@@ -1,9 +1,2 @@
 #include "stdafx.h"
-/*#include "Form1.h" //подключаем заголовки
-#include "Form2.h"
-using namespace Prognoz;
 
-System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
-				 Form2 ^form2 = gcnew Form2();
-				 form2->Show();
-}*/
